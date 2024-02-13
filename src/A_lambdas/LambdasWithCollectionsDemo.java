5dec86a8b301f2d83f6891423fccb279c559ba1b
moshe <moshe.lach@yahoo.com>
2024-02-12 19:01:11 -0500

    Update replaceAll demo

--- src/A_lambdas/LambdasWithCollectionsDemo.java
+++ src/A_lambdas/LambdasWithCollectionsDemo.java
@@ -1,15 +1,13 @@
 package A_lambdas;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class LambdasWithCollectionsDemo {
     public static void main(String[] args) {
-        // forEachDemo();
-        // forEachWithMapDemo();
-        removeIfDemo();
+//         forEachDemo();
+//         forEachWithMapDemo();
+//        removeIfDemo();
         replaceAllDemo();
     }
 
@@ -21,12 +19,16 @@ public class LambdasWithCollectionsDemo {
 
     // demonstrates Map's forEach(BiConsumer action) method
     public static void forEachWithMapDemo() {
-        Map<Integer, String> map = Map.of(4, "four", 6, "six", 2, "two");
+        Map<Integer, String> map = Map.of(4, "a", 6, "b", 2, "c");
         map.forEach((key, val) -> System.out.println(key + " ---- " + val));
     }
 
     // demonstrates Collection's removeIf(Predicate filter) method
     public static void removeIfDemo() {
+        Set<String> set = new HashSet<>(Set.of("alpha", "bravo", "charlie"));
+        set.removeIf(s -> s.length() == 5);
+        System.out.println(set); // [charlie]
+
         List<Integer> list = new ArrayList<>(List.of(-3, -2, -1, 0, 1, 2, 3));
 
         Predicate<Integer> isPositive = x -> x > 0;
@@ -36,13 +38,17 @@ public class LambdasWithCollectionsDemo {
 
         list.removeIf(isPositiveOrEven);
 
-        list.forEach(System.out::println);
+        System.out.println(list); // [-3, -1]
     }
 
     // demonstrates List's replaceAll(UnaryOperator operator)
     public static void replaceAllDemo() {
-        List<String> list = new ArrayList<>(List.of("one", "two", "three"));
-        list.replaceAll(s -> s.toUpperCase());
-        list.forEach(System.out::println);
+        List<String> list = new ArrayList<>(List.of("one", "two"));
+        list.replaceAll(s -> s + " more");
+        System.out.println(list); // [one more, two more]
+        list.replaceAll(String::toUpperCase);
+        System.out.println(list); // [ONE MORE, TWO MORE]
+        list.replaceAll(s -> "a");
+        System.out.println(list); // [a, a]
     }
 }
